4b3b9504959b0e1a13516989508dbfc6921f5956
xabi <xavier.guezala@live.fr>
2020-06-03 11:12:33 +0200

    Dat work

--- src/main/java/com/wildcodeschool/doctor/DoctorWhoApplication.java
+++ src/main/java/com/wildcodeschool/doctor/DoctorWhoApplication.java
@@ -3,11 +3,13 @@ package com.wildcodeschool.doctor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class DoctorWhoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoctorWhoApplication.class, args);
+		run(DoctorWhoApplication.class, args);
 	}
 
 }
